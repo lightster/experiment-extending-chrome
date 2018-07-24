1d0da0e305470f50014d5127aa7a9a0307d9efb0
Matt Light <matt@paren.io>
2018-07-24 15:15:48 -0700

    Make the extension icon duplicate the current tab

--- content.js
+++ content.js
@@ -1,1 +1,12 @@
 console.log("Hello from my Chrome extension")
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.message === "clicked_browser_action") {
+      chrome.runtime.sendMessage({
+        "message": "open_new_tab",
+        "url": location.href
+      })
+    }
+  }
+)
